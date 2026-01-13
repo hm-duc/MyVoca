f3ab8e7fe89e596e69001bed5507e65c75567b07
hmduc <hmduc197@gmail.com>
2026-01-13 13:29:40 +0700

    update export schema for room

--- app/src/main/java/com/hmd/myvoc/data/local/AppDatabase.kt
+++ app/src/main/java/com/hmd/myvoc/data/local/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.hmd.myvoc.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [VocabularyEntity::class], version = 1)
+@Database(entities = [VocabularyEntity::class], version = 1, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
     abstract val vocabularyDao: VocabularyDao
 }
